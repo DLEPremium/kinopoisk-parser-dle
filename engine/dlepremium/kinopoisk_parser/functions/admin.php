5cfcaed20b0c760e642e8e5c507846c60e2cde6d
DLEPremium <109853900+DLEPremium@users.noreply.github.com>
2022-08-08 20:43:07 +0300

    Add files via upload

---
+++ engine/dlepremium/kinopoisk_parser/functions/admin.php
@@ -0,0 +1,275 @@
+<?php
+ 
+if (!defined('DATALIFEENGINE') || !defined('LOGGED_IN')) {
+	die('Hacking attempt!');
+}
+
+function showRow($title = "", $description = "", $field = "", $class = "") {
+	echo "<tr>
+       <td class=\"col-xs-10 col-sm-6 col-md-7 {$class}\"><h6><b>{$title}:</b></h6><span class=\"note large\">{$description}</span></td>
+       <td class=\"col-xs-2 col-md-5 settingstd {$class}\">{$field}</td>
+       </tr>";
+}
+
+function showInput($data)
+{
+    $input_elemet = '';
+	$input_elemet .= $data[3] ? " placeholder=\"{$data[3]}\"" : '';
+	$input_elemet .= $data[4] ? ' disabled' : '';
+	if ($data[1] == 'range') {
+		$class = ' custom-range';
+		$input_elemet .= $data[5] ? " step=\"{$data[5]}\"" : '';
+		$input_elemet .= $data[6] ? " min=\"{$data[6]}\"" : '';
+		$input_elemet .= $data[7] ? " max=\"{$data[7]}\"" : '';
+	} elseif ($data[1] == 'number') {
+		$class = ' w-9';
+		$input_elemet .= isset($data[5]) ? " min=\"{$data[5]}\"" : '';
+		$input_elemet .= $data[6] ? " max=\"{$data[6]}\"" : '';
+	}
+return <<<HTML
+	<input type="{$data[1]}" autocomplete="off" style="float: right;" value="{$data[2]}" class="form-control{$class}" name="{$data[0]}"{$input_elemet}>
+HTML;
+}
+
+function makeCheckBox($name, $selected)
+{
+		$selected = $selected ? "checked" : "";
+		return "<input class=\"switch\" type=\"checkbox\" name=\"{$name}\" value=\"1\" {$selected}>";
+}
+
+function showSelect($name, $value, $check = false)
+{
+	if(!$check) $multiple = "multiple";
+	return "<select data-placeholder=\""."".$phrases_settings['category_chose']."\" name=\"{$name}\" id=\"category\" class=\"valueselect\" {$multiple} style=\"width:100%;max-width:350px;\">{$value}</select>";
+}
+
+function makeDropDown($options, $name, $selected) {
+        $output = "<select class=\"uniform\" style=\"min-width:100px;\" name=\"$name\">\r\n";
+        foreach ( $options as $value => $description ) {
+            $output .= "<option value=\"$value\"";
+            if( $selected == $value ) {
+                $output .= " selected ";
+            }
+            $output .= ">$description</option>\n";
+        }
+        $output .= "</select>";
+        return $output;
+    }
+
+function showTrInline($name, $description, $type, $data)
+{
+echo <<<HTML
+<tr>
+	<td>
+		<label style="float:left;" class="form-label"><b>{$name}</b></label>
+HTML;
+	switch ($type) {
+		case 'input':
+			echo showInput($data);
+		break;
+		case 'textarea':
+			echo textareaForm($data);
+		break;
+		default:
+			echo $data;
+		break;
+	}
+echo <<<HTML
+</tr>
+HTML;
+}
+	
+function textareaForm($data)
+{
+	$input_elemet = $data[2] ? " placeholder=\"{$data[2]}\"" : '';
+	$input_elemet .= $data[3] ? ' disabled' : '';
+return <<<HTML
+	<textarea style="min-height:150px;max-height:150px;min-width:333px;max-width:100%;border: 1px solid #ddd;padding: 5px;" autocomplete="off" class="form-control" name="{$data[0]}"{$input_elemet}>{$data[1]}</textarea>
+HTML;
+}
+
+function ShowSelected($data)
+{
+	foreach ($data[1] as $key => $val) {
+		if ($data[2]) {
+			$output .= "<option value=\"{$key}\"";
+		} else {
+			$output .= "<option value=\"{$val}\"";
+		}
+		if (is_array($data[3])) {
+			foreach ($data[3] as $element) {
+				if ($data[2] && $element == $key) {
+					$output .= " selected ";
+				} elseif (!$data[2] && $element == $val) {
+					$output .= " selected ";
+				}
+			}
+		} elseif ($data[2] && $data[3] == $key) {
+			$output .= " selected ";
+		} elseif (!$data[2] && $data[3] == $val) {
+			$output .= " selected ";
+		}
+		$output .= ">{$val}</option>\n";
+	}
+	$input_elemet = $data[5] ? ' disabled' : '';
+	$input_elemet .= $data[4] ? ' multiple' : '';
+	$input_elemet .= $data[6] ? " data-placeholder=\"{$data[6]}\"" : '';
+return <<<HTML
+<select name="{$data[0]}" class="form-control custom-select" {$input_elemet}>
+	{$output}
+</select>
+HTML;
+}
+
+function makeSelect($array, $name, $data, $placeholder, $mode)
+{
+    $ar_ray = explode(',', $data);
+    foreach ($array as $key => $value) {
+        if ( $mode == 1 ) $key = $value;
+	    if (in_array($key, $ar_ray)) {
+	    	$options[] = '<option value="'.$key.'" selected>'.$value.'</option>';
+	    }
+	    else {
+	    	$options[] = '<option value="'.$key.'">'.$value.'</option>';
+	    }
+    }
+    return '<select data-placeholder="'.$placeholder.'" name="'.$name.'[]" id="'.$name.'" class="valuesselect" multiple style="width:100%;max-width:350px;">'.implode('', $options).'</select>';
+}
+
+$data_list = ['russian', 'original', 'english', 'poster', 'cover', 'logo', 'reviews_count', 'rating_good_review', 'rating_good_review_vote_count', 'rating_kinopoisk', 'votes_kinopoisk', 'rating_imdb', 'votes_imdb', 'rating_film_critics', 'rating_film_critics_vote_count', 'rating_await', 'rating_await_count', 'rating_rf_critics', 'rating_rf_critics_vote_count', 'kinopoisk_url', 'year', 'duration', 'slogan', 'plot', 'short_plot', 'editor_annotation', 'production_status', 'type_en', 'type_ru', 'rating_mpaa', 'rating_age_limits', 'countries', 'genres', 'start_year', 'end_year', 'season', 'episode', 'facts', 'errors', 'world_premier', 'usa_premier', 'russia_premier', 'budget', 'marketing', 'fees_usa', 'fees_world', 'awards', 'youtube_trailer', 'kinopoisk_trailer', 'kinopoisk_teaser', 'directors', 'actors', 'producers', 'screenwriters', 'operators', 'composers', 'design', 'editors', "collections", "screenshot_1", "screenshot_2", "screenshot_3", "screenshot_4", "screenshot_5", "screenshot_6", "screenshot_7", "screenshot_8", "screenshot_9", "screenshot_10", "catalog_ru", "catalog_eng" ];
+
+$xfield_list = xfieldsload();
+
+$xfields_list = ['-' => '-'];
+$xfield_image = ['-' => '-'];
+$xfield_gallery = ['-' => '-'];
+$xfield_yesorno = ['-' => '-'];
+$xfield_select = ['-' => '-'];
+$main_fields = ['title' => '', 
+'short_story' => '',
+'full_story' => '',
+'alt_name' => '',
+'meta_title' => '',
+'meta_description' => '',
+'meta_keywords' => '',
+'tags' => '',
+'catalog' => ''];
+
+for ($i = 0; $i < count($xfield_list); $i++) {
+	if ( $xfield_list[$i][3] == "text" OR $xfield_list[$i][3] == "textarea" OR $xfield_list[$i][3] == "htmljs" ) {
+	    $main_fields[$xfield_list[$i][0]] = $xfield_list[$i][1];
+	    $xfields_list[$xfield_list[$i][0]] = $xfield_list[$i][1];
+	}
+	elseif ( $xfield_list[$i][3] == "image" ) {
+	    $xfield_image[$xfield_list[$i][0]] = $xfield_list[$i][1];
+	}
+	elseif ( $xfield_list[$i][3] == "imagegalery" ) {
+	    $xfield_gallery[$xfield_list[$i][0]] = $xfield_list[$i][1];
+	}
+	elseif ( $xfield_list[$i][3] == "yesorno" ) {
+	    $xfield_yesorno[$xfield_list[$i][0]] = $xfield_list[$i][1];
+	}
+	elseif ( $xfield_list[$i][3] == "select" ) {
+	    $xfield_select[$xfield_list[$i][0]] = $xfield_list[$i][1];
+	}
+}
+
+$screens_count = array("1" => "1 скриншот","2" => "2 скриншота","3" => "3 скриншота","4" => "4 скриншота","5" => "5 скриншотов","6" => "6 скриншотов","7" => "7 скриншотов","8" => "8 скриншотов","9" => "9 скриншотов","10" => "10 скриншотов");
+$choose_date = [
+    0 => '2022-12-31',
+    1 => '31.12.2022',
+    2 => '31 декабря 2022',
+];
+
+$year_array = array();
+$years_array = array();
+for ($i = 1910; $i <= 2025; $i++) {
+    $year_array[$i] = $i.' год';
+    $years_array[] = $i;
+}
+krsort($year_array);
+krsort($years_array);
+
+$country_array = ["Австралия", "Австрия", "Азербайджан", "Албания", "Алжир", "Американское Самоа", "Ангилья", "Англия", "Ангола", "Андорра", "Антигуа и Барбуда", "Аргентина", "Армения", "Аруба", "Афганистан", "Багамы", "Бангладеш", "Барбадос", "Бахрейн", "Бейкер", "Белиз", "Белоруссия", "Бельгия", "Бенилюкс", "Бенин", "Болгария", "Боливия", "Бонэйр", "Бопутатсвана", "Босния и Герцеговина", "Ботсвана", "Бразилия", "Бруней", "Буркина-Фасо", "Бурунди", "Бутан", "Вануату", "Ватикан", "Великобритания", "Венгрия", "Венда", "Венесуэла", "Вьетнам", "Габон", "Гаити", "Гайана", "Гамбия", "Гана", "Гватемала", "Гвинея", "Гвинея-Бисау", "Германия", "Гернси", "Гибралтар", "Гондурас", "Гонконг", "Сомали", "Гренада", "Греция", "Грузия", "Гуам", "Дания", "Конго", "Косово", "Джибути", "Джонстон", "Джубаленд", "Доминика", "Доминикана", "Египет", "Замбия", "Зимбабве", "Израиль", "Имамат Оман", "Индия", "Индонезия", "Иордания", "Ирак", "Иран", "Ирландия", "Исландия", "Испания", "Италия", "Йемен", "Султанат Касири", "Кабо-Верде", "Казахстан", "Камбоджа", "Камерун", "Канада", "Катар", "Кашубия", "Кенедугу", "Кения", "Киргизия", "Кирибати", "Китай", "Колумбия", "Коморы", "Конго", "Корея Северная", "Корея Южная", "Нидерланды", "Конго", "Коста-Рика", "Куба", "Кувейт", "Кюрасао", "Лаос", "Латвия", "Лесото", "Либерия", "Ливан", "Ливия", "Литва", "Лихтенштейн", "Люксембург", "Маврикий", "Мавритания", "Мадагаскар", "Малави", "Малайзия", "Мали", "Мальдивы", "Мальта", "Марокко", "Мартиазо", "Мексика", "Мидуэй", "Мозамбик", "Молдавия", "Молдова", "Монако", "Монголия", "Монтсеррат", "Мьянма", "Намибия", "Науру", "Непал", "Нигер", "Нигерия", "Нидерланды", "Никарагуа", "Ниуэ", "Новая Зеландия", "Новая Каледония", "Норвегия", "Остров Норфолк", "ОАЭ", "Оман", "Пакистан", "Палау", "Панама", "Парагвай", "Перу", "Польша", "Португалия", "Пуэрто Рико", "Ангилья", "Закистан", "Кипр", "Логон", "Россия", "Руанда", "Румыния", "Сальвадор", "Самоа", "Сан-Марино", "Саудовская Аравия", "Северная Ирландия", "Северная Македония", "Сейшельские Острова ", "Сенегал", "Сент-Люсия", "Сербия", "Силенд", "Сингапур", "Синт-Мартен", "Синт-Эстатиус", "Сирия", "Сискей", "Словакия", "Словения", "Соломоновы Острова", "Сомали", "Сомалиленд", "Судан", "Суринам", "СССР", "США", "Сьерра-Леоне", "Таджикистан", "Таиланд", "Тайвань", "Танзания", "Того", "Токелау", "Тонга", "Торо", "Транскей", "Тринидад", "Тобаго", "Тувалу", "Тунис", "Туркмения", "Турция", "Уганда", "Узбекистан", "Украина", "Уругвай", "Уэйк", "Уэльс", "ФШМ", "Фиджи", "Филиппины", "Финляндия", "Фландренсис", "Фолклендские острова", "Франция", "Французская Полинезия", "Хауленд", "Хиршабелле", "Хорватия", "Центральноафриканская Республика", "Чад", "Черногория", "Чехия", "Чили", "Швейцария", "Швеция", "Шотландия", "Шри-Ланка", "Эквадор", "Экваториальная Гвинея", "Эритрея", "Эсватини", "Эстония", "Эфиопия", "Южная Георгия", "ЮАР", "Южный Судан", "Ямайка", "Япония"];
+$type_array = ["Фильм", "Сериал", "ТВ-Шоу"];
+$genres_array = ["аниме", "биография", "боевик", "вестерн", "военный", "детектив", "детский", "для взрослых", "документальный", "драма", "игра", "история", "комедия", "концерт", "короткометражка", "криминал", "мелодрама", "музыка", "мультфильм", "мюзикл", "новости", "приключения", "реальное ТВ", "семейный", "спорт", "ток-шоу", "триллер", "ужасы", "фантастика", "фильм-нуар", "фэнтези", "церемония"];
+$collections_array = ["2х2","Amazon","Apple TV+","BBC","DC","Discovery","Disney","DreamWorks","Fox","HBO","Hulu","KION","Marvel","National Geographic","Netflix","YouTube Premium","Антиутопии","Биографии","Для взрослых","Для женщин","Для молодёжи","Для мужчин","Дорамы","Канал Пятница","Канал Супер","Лаурет премии Оскар","Лучшие фильмы 20 века","Молодежные комедии","Мотивирующие","На реальных событиях","Про агентов","Про акул","Про апокалипсис","Про боевые искусства","Про бывших","Про вампиров","Про ведьм","Про войну 1941-1945","Про гонки","Про девушек","Про детей","Про динозавров","Про докторов","Про драконов","Про животных","Про жизнь","Про звезд","Про зомби","Про инопланетян","Про космос","Про любовь","Про маньяков","Про мафию, банды","Про монстров","Про оборотней","Про ограбления, аферы и мошенников","Про острова","Про подростков","Про полицию","Про призраков","Про путешествия","Про путешествия во времени","Про роботов","Про снайперов","Про спорт","Про средневековье","Про супергероев","Про танки","Про тюрьму","Про футбол","Про школу","Психологические","Рождественские","Романтические комедии","С наградами","С неожиданным концом","СТС","Самые кассовые","Ситкомы","Советские","ТВ3","ТНТ","ТНТ4","Фильмы на Хэллоуин","Фильмы-катастрофы","Экранизация книг"];
+
+$category_values = array_unique(array_merge($type_array,$genres_array));
+$category_values = array_unique(array_merge($category_values,$collections_array));
+$category_values = array_unique(array_merge($category_values,$country_array));
+$category_values = array_unique(array_merge($category_values,$years_array));
+
+$fields_descr = [
+'Русское название',
+'Оригинальное название',
+'Английское название',
+'Ссылка на постер',
+'Ссылка на обложку',
+'Ссылка на логотип',
+'Количество рецензий на КиноПоиск',
+'Рейтинг хороших отзывов на КиноПоиск',
+'Количество хороших отзывов на КиноПоиск',
+'Рейтинг на КиноПоиск',
+'Количество голосов на КиноПоиск',
+'Рейтинг на IMDB',
+'Количество голосов на IMDB',
+'Рейтинг критиков на КиноПоиск',
+'Количество проголосовавших критиков на КиноПоиск',
+'Рейтинг ожиданий на КиноПоиск',
+'Количество ожидающих на КиноПоиск',
+'Рейтинг критиков РФ на КиноПоиск',
+'Количество проголосовавших критиков РФ на КиноПоиск',
+'Ссылка на КиноПоиск',
+'Год выхода',
+'Длительность',
+'Слоган',
+'Сюжет',
+'Краткий сюжет',
+'Заметка редактора',
+'Статус производства',
+'Тип на английском (movie, tvserial, tvshow)',
+'Тип на русском (Фильм, Сериал, ТВ-Шоу)',
+'Рейтинг MPAA',
+'Возрастное ограничение',
+'Страны',
+'Жанры',
+'Год начала показа (для сериалов)',
+'Год окончания показа (для сериалов)',
+'Последний сезон',
+'Последняя серия',
+'Факты',
+'Ошибки',
+'Премьера в мире',
+'Премьера в США',
+'Премьера в РФ',
+'Бюджет',
+'Траты на рекламу',
+'Сборы в США',
+'Сборы в мире',
+'Награды',
+'Трейлер с YouTube',
+'Трейлер с КиноПоиск',
+'Тизер с КиноПоиск',
+'Режиссёры',
+'Актёры',
+'Продюсеры',
+'Сценаристы',
+'Операторы',
+'Композиторы',
+'Художники',
+'Монтажеры',
+'Список коллекций',
+'Ссылка на первый скриншот',
+'Ссылка на второй скриншот',
+'Ссылка на третий скриншот',
+'Ссылка на четвёртый скриншот',
+'Ссылка на пятый скриншот',
+'Ссылка на шестой скриншот',
+'Ссылка на седьмой скриншот',
+'Ссылка на восьмой скриншот',
+'Ссылка на девятый скриншот',
+'Ссылка на десятый скриншот',
+'Первая буква русского названия',
+'Первая буква оригинального названия'
+];
---
+++ engine/dlepremium/kinopoisk_parser/functions/module.php
@@ -0,0 +1,262 @@
+<?php
+
+if (!function_exists('api_request')) {
+    function api_request($url, $x_api_key) {
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $headers = [
+    		'Content-Type: application/json',
+    		'X-API-KEY: '.$x_api_key
+		];
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$kp_api = curl_exec ($ch);
+		curl_close ($ch);
+  
+  		return json_decode($kp_api, true);
+    }
+}
+
+if (!function_exists('check_if')) {
+    function check_if($check_value, $dataArray) {
+        $tags_array = array();
+        foreach($dataArray as $named => $zna4enie) {
+            if (strpos($check_value, '[if_'.$named.']') !== false) {
+                if ($zna4enie) $check_value = preg_replace(';\[if_'.$named.'\](.*?)\[\/if_'.$named.'\];is', '$1', $check_value);
+                else $check_value = preg_replace(';\[if_'.$named.'\](.*?)\[\/if_'.$named.'\];is', '', $check_value);
+            }
+            if (strpos($check_value, '[ifnot_'.$named.']') !== false) {
+                if ($zna4enie) $check_value = preg_replace(';\[ifnot_'.$named.'\](.*?)\[\/ifnot_'.$named.'\];is', '', $check_value);
+                else $check_value = preg_replace(';\[ifnot_'.$named.'\](.*?)\[\/ifnot_'.$named.'\];is', '$1', $check_value);
+            }
+            $tags_array[] = '{'.$named.'}';
+        }
+        $check_value = str_ireplace( $tags_array, $dataArray, $check_value);
+    	return $check_value;
+    }
+}
+
+if (!function_exists('xfieldsdatasaved')) {
+    function xfieldsdatasaved($xfields) {
+        $filecontents = [];
+        foreach ($xfields as $xfielddataname => $xfielddatavalue) {
+            if ($xfielddatavalue === '') continue;
+            $xfielddataname = str_replace( "|", "&#124;", $xfielddataname);
+            $xfielddataname = str_replace( "\r\n", "__NEWL__", $xfielddataname);
+            $xfielddatavalue = str_replace( "|", "&#124;", $xfielddatavalue);
+            $xfielddatavalue = str_replace( "\r\n", "__NEWL__", $xfielddatavalue);
+            $filecontents[] = $xfielddataname."|".$xfielddatavalue;
+        }
+        $filecontents = join('||', $filecontents );
+        return $filecontents;
+    }
+}
+
+if (!function_exists('xfparamload')) {
+    function xfparamload( $xfname ) {
+        $path = ENGINE_DIR . '/data/xfields.txt';
+        $filecontents = file( $path );
+        
+        foreach ( $filecontents as $name => $value ) {
+            $filecontents[$name] = explode( "|", trim( $value ) );
+            if($filecontents[$name][0] == $xfname ) return $filecontents[$name];
+        }
+        return false;
+    }    
+}
+
+if (!function_exists('setPoster')) {
+function setPoster($poster_url, $poster_title, $poster_name = false, $news_id = 0) {
+	
+	global $config, $db, $member_id, $user_group;
+	
+	$area = 'xfieldsimage';
+	
+	if ( $poster_name ) {
+		$xfparam = xfparamload($poster_name);
+	}
+	else $xfparam = [];
+	
+	$xfname = $xfparam[0];
+	$t_seite = (int)$config['t_seite'];
+	$m_seite = $t_seite;
+	$t_size = $xfparam[13];
+	$m_size = 0;
+	$config['max_up_side'] = $xfparam[9];
+	$config['max_up_size'] = $xfparam[10];
+	$config['min_up_side'] = $xfparam[22];
+	$make_watermark = (bool)$xfparam[11];
+	$make_thumb = (bool)$xfparam[12];
+	$make_medium = false;
+
+	$t_size = explode("x", $t_size);
+	if (count($t_size) == 2) {
+		$t_size = (int)$t_size[0] . "x" . (int)$t_size[1];
+	} else $t_size = (int)$t_size[0];
+
+	$m_size = explode("x", $m_size);
+	if (count($m_size) == 2) {
+		$m_size = (int)$m_size[0] . "x" . (int)$m_size[1];
+	} else $m_size = (int)$m_size[0];
+
+    $author = $db->safesql($member_id['name']);
+            
+    $poster_data = file_get_contents($poster_url);
+            
+    $poster_title = totranslit(stripslashes( $poster_title ), true, false) . '.jpg';
+            
+    $new_poster = ROOT_DIR . '/uploads/files/' . $poster_title;
+            
+    file_put_contents($new_poster, $poster_data);
+            
+    $exif = exif_read_data($new_poster);
+
+            
+    $_FILES['qqfile'] = [
+        'type' => $exif['MimeType'],
+        'name' => $exif['FileName'],
+        'tmp_name' => $new_poster,
+        'error' => 0,
+        'size' => $exif['FileSize']
+    ];
+
+            
+    $uploader = new FileUploader($area, $news_id, $author, $t_size, $t_seite, $make_thumb, $make_watermark, $m_size, $m_seite, $make_medium);
+    $result = json_decode($uploader->FileUpload(), true);
+
+    @unlink($new_poster);
+    return $result;
+}
+}
+
+if (!function_exists('totranslit_it')) {
+    function totranslit_it($var, $lower = true, $punkt = true) {
+	
+$langtranslit = array(
+	'а' => 'a', 'б' => 'b', 'в' => 'v',
+	'г' => 'g', 'д' => 'd', 'е' => 'e',
+	'ё' => 'e', 'ж' => 'zh', 'з' => 'z',
+	'и' => 'i', 'й' => 'j', 'к' => 'k',
+	'л' => 'l', 'м' => 'm', 'н' => 'n',
+	'о' => 'o', 'п' => 'p', 'р' => 'r',
+	'с' => 's', 'т' => 't', 'у' => 'u',
+	'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+	'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch',
+	'ь' => '', 'ы' => 'y', 'ъ' => '',
+	'э' => 'je', 'ю' => 'ju', 'я' => 'ja',
+	"ї" => "ji", "є" => "ye", "ґ" => "g",
+	
+	'А' => 'A', 'Б' => 'B', 'В' => 'V',
+	'Г' => 'G', 'Д' => 'D', 'Е' => 'E',
+	'Ё' => 'E', 'Ж' => 'Zh', 'З' => 'Z',
+	'И' => 'I', 'Й' => 'J', 'К' => 'K',
+	'Л' => 'L', 'М' => 'M', 'Н' => 'N',
+	'О' => 'O', 'П' => 'P', 'Р' => 'R',
+	'С' => 'S', 'Т' => 'T', 'У' => 'U',
+	'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+	'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sch',
+	'Ь' => '', 'Ы' => 'Y', 'Ъ' => '',
+	'Э' => 'Je', 'Ю' => 'Ju', 'Я' => 'Ja',
+	"Ї" => "Ji", "Є" => "ye", "Ґ" => "G",
+	"À"=>"A", "à"=>"a", "Á"=>"A", "á"=>"a", 
+	"Â"=>"A", "â"=>"a", "Ä"=>"A", "ä"=>"a", 
+	"Ã"=>"A", "ã"=>"a", "Å"=>"A", "å"=>"a", 
+	"Æ"=>"AE", "æ"=>"ae", "Ç"=>"C", "ç"=>"c", 
+	"Ð"=>"D", "È"=>"E", "è"=>"e", "É"=>"E", 
+	"é"=>"e", "Ê"=>"E", "ê"=>"e", "Ì"=>"I", 
+	"ì"=>"i", "Í"=>"I", "í"=>"i", "Î"=>"I", 
+	"î"=>"i", "Ï"=>"I", "ï"=>"i", "Ñ"=>"N", 
+	"ñ"=>"n", "Ò"=>"O", "ò"=>"o", "Ó"=>"O", 
+	"ó"=>"o", "Ô"=>"O", "ô"=>"o", "Ö"=>"O", 
+	"ö"=>"o", "Õ"=>"O", "õ"=>"o", "Ø"=>"O", 
+	"ø"=>"o", "Œ"=>"OE", "œ"=>"oe", "Š"=>"S", 
+	"š"=>"s", "Ù"=>"U", "ù"=>"u", "Û"=>"U", 
+	"û"=>"u", "Ú"=>"U", "ú"=>"u", "Ü"=>"U", 
+	"ü"=>"u", "Ý"=>"Y", "ý"=>"y", "Ÿ"=>"Y", 
+	"ÿ"=>"y", "Ž"=>"Z", "ž"=>"z", "Þ"=>"B", 
+	"þ"=>"b", "ß"=>"ss", "£"=>"pf", "¥"=>"ien", 
+	"І"=>"I", "і"=>"i", "ð"=>"eth", "ѓ"=>"r"
+);
+	
+	    if ( is_array($var) ) return "";
+
+	    $var = str_replace(chr(0), '', $var);
+	
+	    $var = trim( strip_tags( $var ) );
+	    $var = preg_replace( "/\s+/u", "-", $var );
+	    $var = str_replace( "/", "-", $var );
+	
+	    if (is_array($langtranslit) AND count($langtranslit) ) {
+	    	$var = strtr($var, $langtranslit);
+	    }
+
+	    if ( $punkt ) $var = preg_replace( "/[^a-z0-9\_\-.]+/mi", "", $var );
+	    else $var = preg_replace( "/[^a-z0-9\_\-]+/mi", "", $var );
+
+	    $var = preg_replace( '#[\-]+#i', '-', $var );
+	    $var = preg_replace( '#[.]+#i', '.', $var );
+
+	    if ( $lower ) $var = strtolower( $var );
+
+	    $var = str_ireplace( ".php", "", $var );
+	    $var = str_ireplace( ".php", ".ppp", $var );
+
+	    if( strlen( $var ) > 200 ) {
+		
+	    	$var = substr( $var, 0, 200 );
+		
+	    	if( ($temp_max = strrpos( $var, '-' )) ) $var = substr( $var, 0, $temp_max );
+	
+	    }
+	
+	    return $var;
+    }
+}
+
+function convert_date($date, $type) {
+    if ( $type == 0 ) return $date;
+    elseif ( $type == 1 ) {
+        $date_mas = explode("-", $date);
+        return $date_mas[2].".".$date_mas[1].".".$date_mas[0];
+    }
+    elseif ( $type == 2 ) {
+        $date_mas = explode("-", $date);
+        $month_mas = [
+            "01" => " января ",
+            "02" => " февраля ",
+            "03" => " марта ",
+            "04" => " апреля ",
+            "05" => " мая ",
+            "06" => " июня ",
+            "07" => " июля ",
+            "08" => " августа ",
+            "09" => " сентября ",
+            "10" => " октября ",
+            "11" => " ноября ",
+            "12" => " декабря ",
+        ];
+        return intval($date_mas[2]).$month_mas[$date_mas[1]].$date_mas[0];
+    }
+}
+
+function in_arrayi($needle, $haystack) {
+    return in_array(strtolower($needle), array_map('strtolower', $haystack));
+}
+
+function unique_multidim_array($array, $key) {
+    $temp_array = array();
+    $i = 0;
+    $key_array = array();
+   
+    foreach($array as $val) {
+        if (!in_array($val[$key], $key_array)) {
+            $key_array[$i] = $val[$key];
+            $temp_array[$i] = $val;
+        }
+        $i++;
+    }
+    return $temp_array;
+}
